caa07034d6c946757bbc1e6d6d6fc55ba378e25b
Maria Gil <maria.gilaragones@gmail.com>
2018-10-19 16:32:48 +0200

    Code cleaning

--- traffic_signs/morphology_utils.py
+++ traffic_signs/morphology_utils.py
@@ -11,11 +11,13 @@ def fill_holes(mask):
     cv2.floodFill(im_floodfill, filling_mask, (0, 0), 1)
     return mask.astype(np.uint8) | (1 - im_floodfill)
 
+
 def filter_noise(mask):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
     mask = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_OPEN, kernel)
     mask = cv2.medianBlur(mask, 7)
     return mask
+
 
 def granulometry(mask, steps, dict_kernels):
     """
@@ -23,10 +25,10 @@ def granulometry(mask, steps, dict_kernels):
     """
     new_mask = copy.deepcopy(mask.astype(np.uint8))
     g_curve = np.zeros(steps)
-    for i in range(steps-1):
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (i+1, i+1))
+    for i in range(steps - 1):
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (i + 1, i + 1))
         remain = cv2.morphologyEx(new_mask, cv2.MORPH_OPEN, kernel)
-        g_curve[i+1] = np.sum(np.abs(np.count_nonzero(remain)))
+        g_curve[i + 1] = np.sum(np.abs(np.count_nonzero(remain)))
         new_mask = remain
 
     g_curve[0] = g_curve[1]
@@ -35,10 +37,10 @@ def granulometry(mask, steps, dict_kernels):
     plt.plot(pecstrum)
 
     for i in range(2):
-        peak = np.where(pecstrum==max(pecstrum))[0][0]
+        peak = np.where(pecstrum == max(pecstrum))[0][0]
         if peak in dict_kernels.keys():
-            dict_kernels[peak]+=1
+            dict_kernels[peak] += 1
         else:
-            dict_kernels[peak]=1
+            dict_kernels[peak] = 1
         pecstrum[peak] = min(pecstrum)
     return dict_kernels
