7b4c88ee442b7e606939f2afb57d5958ede22935
Oscar Mañas <oscmansan@gmail.com>
2018-10-27 15:48:02 +0200

    Add retrieval system

--- painting_retrieval/src/main.py
+++ painting_retrieval/src/main.py
@@ -1,20 +1,31 @@
 import glob
 
-import numpy as np
 import imageio
+from matplotlib import pyplot as plt
 
-from descriptors import descriptor
-from similarity_measures import similarity
+from retrieval import query_batch
+from timer import Timer
 
 
 def main():
-    image = imageio.imread(np.random.choice(glob.glob('../data/museum_set_random/*.jpg')))
-    query = imageio.imread(np.random.choice(glob.glob('../data/query_devel_random/*.jpg')))
+    query_files = sorted(glob.glob('../data/query_devel_random/*.jpg'))
+    image_files = sorted(glob.glob('../data/museum_set_random/*.jpg'))
 
-    u = descriptor(image)
-    v = descriptor(query)
+    method = 'pyramid_hsv_histogram'
+    metric = 'euclidean_distance'
+    with Timer('query_batch'):
+        results = query_batch(query_files, image_files, method, metric)
 
-    print(similarity(u, v))
+    for query_file, result in zip(query_files, results):
+        print('(query) {}'.format(query_file))
+        for image_file, dist in result:
+            print('({:.6f}) {}'.format(dist, image_file))
+
+        #plt.figure()
+        #plt.imshow(imageio.imread(query_file))
+        #plt.figure()
+        #plt.imshow(imageio.imread(result[0][0]))
+    #plt.show()
 
 
 if __name__ == '__main__':
